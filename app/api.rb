db5571016fdf32924dfdca6782ae560430b5d201
isarceaux <isarceaux>
2017-12-19 16:23:41 +0100

    deleted the test file for json

--- app/api.rb
+++ app/api.rb
@@ -1,10 +1,28 @@
 require 'bundler/setup'
 require 'sinatra/base'
+require 'json'
 
 class Api < Sinatra::Base
 
+  get '/' do
+    'hello world!'
+  end
+
 	get '/hello' do
-		'hello world!'
+		'Hello world!'
 	end
+
+  get '/movies' do
+    content_type :json
+    a = JSON.parse(File.read('data.json'))
+    a.fetch("movies").to_json
+  end
+
+  get '/movies/:id' do
+    content_type :json
+    a = JSON.parse(File.read('data.json'))
+    a.fetch("movies")[(params[:id].to_i)-1].to_json
+
+  end
 
 end
